6dfbcda9a24c403b67cf2b1e6fd1cf8cccfeb087
deebhikakumaran <deebhika04@gmail.com>
2026-06-08 15:25:27 +0000

    lets see

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT ?? 8000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json({ message: 'server is healthy', status: 'verifying pm2 deployment and github actions sync.', update: 'updated public key in remote machine'});
+    res.json({ message: 'server is healthy', status: 'verifying pm2 deployment and github actions sync.', update: '--BEGIN--END'});
 });
 
 app.listen(PORT, () => {
